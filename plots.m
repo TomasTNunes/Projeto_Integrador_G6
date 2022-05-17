bd32d440b07aefb641b8dfe8099a1771642da9bd
TomasTNunes <tomastrindadenunes@outlook.com>
2022-05-18 00:03:49 +0100

    plot

--- plots.m
+++ plots.m
@@ -8,6 +8,8 @@ fig_n = 1;
 %% raio rotores, numero rotores, W/A
 clearvars -except fig_n filename
 xlRange = 'B8:F19';
+xlRange_var = 'B7:F7';
+[~,data_varname] = xlsread(filename,xlRange_var);
 data = xlsread(filename,xlRange);
 data_X = data(:,1);
 data_Y = data(:,2);
@@ -30,12 +32,17 @@ end
 figure(fig_n)
 surf(X,Y,Z)
 colorbar
+xlabel(data_varname{1})
+ylabel(data_varname{2})
+zlabel(data_varname{5})
 fig_n = fig_n + 1;
 
 
 %% h vertical climb, velocidade, W/S
 clearvars -except fig_n filename
 xlRange = 'K8:O31';
+xlRange_var = 'K7:O7';
+[~,data_varname] = xlsread(filename,xlRange_var);
 data = xlsread(filename,xlRange);
 data_X = data(:,1);
 data_Y = data(:,2);
@@ -58,8 +65,10 @@ end
 figure(fig_n)
 surf(X,Y,Z)
 colorbar
+xlabel(data_varname{1})
+ylabel(data_varname{2})
+zlabel(data_varname{5})
 fig_n = fig_n + 1;
-
 % plot(data_Y(1:6),data_Z(1:6))
 % hold on
 % plot(data_Y(7:12),data_Z(7:12))
