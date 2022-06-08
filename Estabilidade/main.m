308c61e86eb60c0eb6285f4feb4a6bb6e77a897c
TomasTNunes <tomastrindadenunes@outlook.com>
2022-06-08 17:32:19 +0100

    CG

--- Estabilidade/main.m
+++ Estabilidade/main.m
@@ -10,9 +10,11 @@ filename = 'Centros_Geometricos.xlsx';
 % get parts names
 xlRange_comp = 'A2:A16';
 [~,comp] = xlsread(filename,xlRange_comp);
+
 % get parts json names
 xlRange_jname = 'E2:E16';
 [~,jname] = xlsread(filename,xlRange_jname);
+
 % get coordinates
 xlRange = 'B2:D16';
 data = xlsread(filename,xlRange);
@@ -36,7 +38,7 @@ Y = rot_coord(2,:);
 Z = rot_coord(3,:);
 
 % Plot geometric Centers of components
-plot_geo_centers(X,Y,Z,comp)
+plot_centers(X,Y,Z,comp)
 
 clearvars -except X Y Z comp jname
 
@@ -44,9 +46,45 @@ clearvars -except X Y Z comp jname
 % Get data from json and algorithm
 global constants;
 constants.g = 9.81; % m/s^2
-data = load_project('transport.json');
+data = load_project('rescue.json');
 data.mission = build_mission(data.mission);
 data.vehicle = build_vehicle(data.mission, data.vehicle);
 data.vehicle = aero_analysis(data.mission, data.vehicle);
 [data.mission, data.vehicle] = mass_analysis(data.mission, data.vehicle, data.energy);
 
+% Get mass for each component
+comp_mass(1) = 0;
+for i=2:length(jname)
+    clear elem id
+    if strcmp(jname{i}, 'Tail')
+        jname_t1 = 'Vertical Tail';
+        jname_t2 = 'Horizontal Tail';
+        [elem1, id1] = find_by_name(data.vehicle.components, jname_t1);
+        [elem2, id2] = find_by_name(data.vehicle.components, jname_t2);
+        comp_mass(i) = elem1.mass + elem2.mass;
+        clear jname_t1 jname_t2 elem1 elem2 id1 id2
+    else
+        [elem, id] = find_by_name(data.vehicle.components, jname{i});
+        comp_mass(i) = elem.mass;
+    end
+end
+
+%% Compute Center of Mass
+% sum of masses of all components
+Total_mass = sum(comp_mass(2:end));
+
+% calculate Center of Mass
+X_CG = sum(X(2:end).*comp_mass(2:end)) / Total_mass;
+Y_CG = sum(Y(2:end).*comp_mass(2:end)) / Total_mass;
+Z_CG = sum(Z(2:end).*comp_mass(2:end)) / Total_mass;
+
+% Plot geometric Centers of components and Center of Mass
+X_aux = X;
+Y_aux = Y;
+Z_aux = Z;
+comp_aux = comp;
+X_aux(end+1) = X_CG;
+Y_aux(end+1) = Y_CG;
+Z_aux(end+1) = Z_CG;
+comp_aux{end+1} = 'CG';
+plot_centers(X_aux,Y_aux,Z_aux,comp_aux)
--- Estabilidade/plot_centers.m
+++ Estabilidade/plot_centers.m
@@ -1,10 +1,14 @@
-function plot_geo_centers(X,Y,Z,comp)
+function plot_centers(X,Y,Z,comp)
 
+w_CG = 0;
 plotColors = jet(length(X));
 figure()
 for ii=1:length(X)
     if ii==1
         plot3(X(ii),Y(ii),Z(ii),'xk','LineWidth', 1.5)
+    elseif strcmp(comp(ii),'CG')
+        plot3(X(ii),Y(ii),Z(ii),'*k','LineWidth', 1.8)
+        w_CG = 1;
     else
         plot3(X(ii),Y(ii),Z(ii),'o','Color', plotColors(ii,:), 'LineWidth', 1.5)
     end
@@ -17,7 +21,11 @@ ylabel('Y [mm]')
 zlabel('Z [mm]')
 set ( gca,'ydir','reverse')
 set ( gca,'zdir','reverse')
-title('Centros Geometricos das Componentes')
+if w_CG
+    title('Centros Geometricos das Componentes e CG')
+else
+    title('Centros Geometricos das Componentes')
+end
 legend(comp)
 
 end
